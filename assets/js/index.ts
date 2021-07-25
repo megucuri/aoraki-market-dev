195b1f0e9b36fb94a54b395727502ae00acb1cfd
yosi <yosi@yosinoMacBook-Air.local>
2021-07-25 20:02:31 +0900

    Changed baseURL to relativeURL is True

--- assets/js/index.ts
+++ assets/js/index.ts
@@ -29,3 +29,14 @@
 })(jQuery);
 
 
+AOS.init();
+AOS.init({
+    // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+    offset: 200, // offset (in px) from the original trigger point
+    delay: 50, // values from 0 to 3000, with step 50ms
+    duration: 1000, // values from 0 to 3000, with step 50ms
+    easing: 'ease-in-out', // default easing for AOS animations
+    once: false, // whether animation should happen only once - while scrolling down
+    mirror: true, // whether elements should animate out while scrolling past them
+    anchorPlacement: 'top-center', // defines which position of the element regarding to window should trigger the animation
+});
